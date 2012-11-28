6e726c91554e943ef291376ba4dc133efac597ac
sodcheats <sodcheats@gmail.com>
2012-11-28 16:08:42 -0600

    more updates again

--- _compile.bat
+++ _compile.bat
@@ -1,3 +1,3 @@
 g++ --version
-g++ eqmap.cpp -D FREEGLUT_STATIC -Id:\code\boost\include -Id:\code\everquest\eqmac\include -Ld:\code\boost\lib -lfreeglut_static -lglu32 -lopengl32 -lwinmm -lgdi32 -lkernel32 -luser32 -Wl,--enable-auto-import -Wl,-subsystem,windows -Wall -O0 -s -o release/eqmap.exe
+g++ -D FREEGLUT_STATIC eqmap.cpp -Id:\code\boost\include -Id:\code\everquest\eqmac\include -Ld:\code\boost\lib -static -lfreeglut_static -lglu32 -lopengl32 -lwinmm -lgdi32 -lkernel32 -luser32 -Wl,--enable-auto-import -Wl,-subsystem,windows -Wall -O0 -s -o release/eqmap.exe
 pause
--- eqmap.cpp
+++ eqmap.cpp
@@ -37,9 +37,9 @@ std::vector<std::string> maps;
 float map_origin_x = window_width  / 2;
 float map_origin_y = window_height / 2;
 
-float MAP_ZOOM_DEFAULT = 1.0;
+const float MAP_ZOOM_DEFAULT = 1.0;
 
-float MAP_ZOOM_MIN = 0.1;
+const float MAP_ZOOM_MIN = 0.1;
 
 float map_zoom = MAP_ZOOM_DEFAULT;
 
@@ -56,17 +56,17 @@ float map_draw_y = 0;
 bool map_draw_info_text = true;
 
 /*
-    GLUT_BITMAP_9_BY_15
-    GLUT_BITMAP_8_BY_13
-    GLUT_BITMAP_HELVETICA_18
-    GLUT_BITMAP_HELVETICA_12
-    GLUT_BITMAP_HELVETICA_10
-    GLUT_BITMAP_TIMES_ROMAN_24
-    GLUT_BITMAP_TIMES_ROMAN_10
+GLUT_BITMAP_9_BY_15
+GLUT_BITMAP_8_BY_13
+GLUT_BITMAP_HELVETICA_18
+GLUT_BITMAP_HELVETICA_12
+GLUT_BITMAP_HELVETICA_10
+GLUT_BITMAP_TIMES_ROMAN_24
+GLUT_BITMAP_TIMES_ROMAN_10
 */
 void *font_name = GLUT_BITMAP_HELVETICA_10;
 
-int font_size = 10; // glutBitmapHeight(font_name); crashes
+int font_size = 10;
 
 int num_lines    = 0;
 int num_lines_ex = 0;
@@ -715,6 +715,7 @@ int main(int argc, char** argv)
     glutMotionFunc(motion);
 
     init();
+
     parse(map_filename);
 
     glutMainLoop();
